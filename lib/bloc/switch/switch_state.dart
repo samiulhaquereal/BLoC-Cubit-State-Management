6eb4ce8e2a46e597c4200213240f0e891993e78e
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-08-28 11:25:01 +0600

    6th

--- lib/bloc/switch/switch_state.dart
+++ lib/bloc/switch/switch_state.dart
@@ -1,15 +1,21 @@
 import 'package:equatable/equatable.dart';
 
 class SwitchState extends Equatable{
-  SwitchState({this.isSwitch = false});
+  SwitchState({
+    this.isSwitch = false,
+    this.slider = 1.0
+  });
 
   bool isSwitch;
-  SwitchState copyWith({bool? isSwitch}){
+  double slider;
+
+  SwitchState copyWith({bool? isSwitch,double? slider}){
     return SwitchState(
-        isSwitch: isSwitch ?? this.isSwitch
+        isSwitch: isSwitch ?? this.isSwitch,
+        slider: slider ?? this.slider
     );
 }
 
   @override
-  List<Object> get props => [isSwitch];
+  List<Object> get props => [isSwitch,slider];
 }
--- lib/view/switch_view.dart
+++ lib/view/switch_view.dart
@@ -1,4 +1,8 @@
+import 'package:blocstatemanagement/bloc/switch/switch_bloc.dart';
+import 'package:blocstatemanagement/bloc/switch/switch_event.dart';
+import 'package:blocstatemanagement/bloc/switch/switch_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SwitchScreen extends StatelessWidget {
   const SwitchScreen({super.key});
@@ -6,26 +10,32 @@ class SwitchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(child: Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: BlocBuilder<SwitchBloc, SwitchState>(
+        builder: (context, state) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Notification Enable'),
-              Switch(value: true, onChanged: (value){})
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('Notification Enable'),
+                  Switch(value: state.isSwitch, onChanged: (value) {
+                    context.read<SwitchBloc>().add(NotificationEnableAndDisable());
+                  })
+                ],
+              ),
+              SizedBox(height: 20),
+              Container(
+                color: Colors.red.withOpacity(0.5),
+                height: 200,
+                width: 200,
+              ),
+              SizedBox(height: 20),
+              Slider(value: 0.5, onChanged: (vale) {})
             ],
-          ),
-          SizedBox(height: 20),
-          Container(
-            color: Colors.red.withOpacity(0.5),
-            height: 200,
-            width: 200,
-          ),
-          SizedBox(height: 20),
-          Slider(value: 0.5, onChanged: (vale){})
-        ],
+          );
+        },
       ),
     ));
   }
